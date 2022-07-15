9d01c98638d7fefc120175a6036114807987a4d3
Denis Stoyanouski <vandenst@tut.by>
2022-07-15 08:55:50 +0300

    add four methods

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -41,61 +41,46 @@ public class Main {
     }
 
     public static void choiceCell() {
-        String input;
-        String first = "";
-        String second ="";
+        int first = 0;
+        int second = 0;
         do {
             System.out.print("Enter the coordinates: ");
-            input = scanner.nextLine().trim();
-            if (input.indexOf(" ") != -1) {
-                first = input.substring(0, input.indexOf(" "));
-                second = input.substring(input.indexOf(" ") + 1);
-            } else {
+            try {
+                first = scanner.nextInt();
+                second = scanner.nextInt();
+            } catch (Exception e) {
+                e.getMessage();
                 System.out.println("You should enter numbers!");
             }
+
         } while (!checkInput(first, second));
 
     }
 
-    public static boolean checkInput(String first, String second) {
+    public static boolean checkInput(int first, int second) {
 
-        int cellRow = 0;
-        int cellColumn = 0;
         boolean firstCheck = false;
         boolean secondCheck = false;
-        int[] coordinate = new int[2];
-        if (!first.isEmpty() && first.matches("\\d+")) {
-            cellRow = Integer.parseInt(first);
-            if (cellRow >= 1 && cellRow <= 3 ) {
+            if (first >= 1 && first <= 3 ) {
                 firstCheck = true;
             } else {
                 System.out.println("Coordinates should be from 1 to 3!");
                 firstCheck = false;
             }
-        } else {
-            System.out.println("You should enter numbers!");
-            firstCheck = false;
-        }
 
-        if (!second.isEmpty() && second.matches("\\d+")) {
-            cellColumn = Integer.parseInt(first);
-            if (cellColumn >= 1 && cellColumn <= 3 ) {
+            if (second >= 1 && second <= 3 ) {
                 secondCheck = true;
             } else {
                 System.out.println("Coordinates should be from 1 to 3!");
                 secondCheck = false;
             }
-        } else {
-            System.out.println("You should enter numbers!");
-            secondCheck = true;
-        }
-        checkPlacement(cellRow, cellColumn);
-        return firstCheck && secondCheck && checkPlacement(cellRow, cellColumn);
+        checkPlacement(first, second);
+        return firstCheck && secondCheck && checkPlacement(first, second);
     }
 
-    private static boolean checkPlacement(int cellRow, int cellColumn) {
+    private static boolean checkPlacement(int first, int second) {
         boolean check = false;
-        if (table[cellRow][cellColumn] != "_") {
+        if (table[first - 1][second - 1] != "_") {
             System.out.println("This cell is occupied! Choose another one!");
         } else {
             check = true;
